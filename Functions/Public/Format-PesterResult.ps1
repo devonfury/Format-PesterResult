923f1d4d50e7db0cb32a225ee8bd1fb1d2c33ef7
Marvin Carpenter <marvin.carpenter@gmail.com>
2019-05-19 11:35:59 -0500

    updated

--- Functions/Public/Format-PesterResult.ps1
+++ Functions/Public/Format-PesterResult.ps1
@@ -10,8 +10,12 @@ function Format-PesterResult
         The Pester TestResult object.
         .PARAMETER ReportPath
         The path that where the CSV should be saved.
+        .PARAMETER CustomHeader
+        Replace the Test Name in the Pester Result object with custom names.
         .EXAMPLE
         Format-PesterResult -PesterResult $myTestResult -ReportPath 'c:\myreport.csv'
+        .EXAMPLE
+        Format-PesterResult -PesterResult $myTestResult -CustomHeader @('Column1','Column2','Column3') -ReportPath 'c:\myreport.csv'
         .INPUTS
         None
         .OUTPUTS
@@ -24,13 +28,24 @@ function Format-PesterResult
         [parameter(Mandatory=$true,ValueFromPipeline,Position=0)]
         [pscustomobject[]] $PesterResult,
         [parameter(Mandatory=$false)]   
-        [string] $ReportPath   
+        [string] $ReportPath,
+        [parameter(Mandatory=$false)]   
+        [string[]] $CustomHeader   
     )
  
     try
     {   
-        $headerArray = CreateHeaderArray       
-        $header = WriteHeader -headerArray $headerArray        
+        $headerArray = CreateHeaderArray
+ 
+        if($PSBoundParameters.ContainsKey('CustomHeader'))
+        {
+            $header = WriteHeader -headerArray $headerArray -CustomArray $CustomHeader        
+        }
+        else
+        {       
+            $header = WriteHeader -headerArray $headerArray       
+        }                  
+ 
         $detail = WriteDetail -TestCount $headerArray.Count
         $report = -join($header,$detail)
         #write-warning $report
@@ -41,8 +56,6 @@ function Format-PesterResult
         }
         else
         {
-            $report | ConvertFrom-Csv
-            <#
             $tempPath = -join($env:USERPROFILE,"\Documents\pester$((Get-Date).ToFileTimeUtc()).csv")   
             New-Item -Path $tempPath -ItemType File -Value $report -Force | Out-Null
             if(Test-Path -Path $tempPath)
@@ -51,7 +64,6 @@ function Format-PesterResult
                 write-output $objReport       
                 Remove-Item -Path $tempPath
             }
-            #>
         }
     }
     catch
--- Tests/ManifestTest.tests.ps1
+++ Tests/ManifestTest.tests.ps1
@@ -1,45 +1,48 @@
-$ModulePath = Split-Path -Parent $MyInvocation.MyCommand.Path
-$ModuleName = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -Replace ".Tests.ps1"
-
-$ManifestPath   = "$ModulePath\$ModuleName.psd1"
-
 # test the module manifest - exports the right functions, processes the right formats, and is generally correct
-Describe "Manifest" {
-    $Manifest = $null
-    It "has a valid manifest" {
-        {
-            $Script:Manifest = Test-ModuleManifest -Path $ManifestPath -ErrorAction Stop -WarningAction SilentlyContinue
-        } | Should Not Throw
+$path = $MyInvocation.MyCommand.Path
+$ModulePath = (Split-Path -Path $path -Parent) | Split-Path -Parent #Split-Path -Parent $MyInvocation.MyCommand.Path
+$ModuleName = Split-Path -Path $ModulePath -Leaf #(Split-Path -Leaf $MyInvocation.MyCommand.Path) -Replace ".Tests.ps1"
+$ManifestPath   = "$ModulePath\$ModuleName.psd1"
+$ManifestHash = Invoke-Expression (Get-Content $ManifestPath -Raw)
+
+Describe "Manifest" {			
+	It "has a valid manifest" {
+		{
+			$null = Test-ModuleManifest -Path $ManifestPath -ErrorAction Stop -WarningAction SilentlyContinue
+		} | Should Not Throw
     }
 
     It "has a valid name" {
-        $Script:Manifest.Name | Should Be $ModuleName
+        $ManifestHash.Name | Should Be $ModuleName
     }
 
 	It "has a valid root module" {
-        $Script:Manifest.RootModule | Should Be "$ModuleName.psm1"
+        $ManifestHash.RootModule | Should Be "$ModuleName.psm1"
     }
 
 	It "has a valid Description" {
-        $Script:Manifest.Description | Should Not BeNullOrEmpty
+        $ManifestHash.Description | Should Not BeNullOrEmpty
     }
 
     It "has a valid guid" {
-        $Script:Manifest.Guid | Should Be '9dd7e9a4-8525-4fd1-aa13-3a063df4b264'
+        [Guid]::Parse($ManifestHash.Guid) | Should Not BeNullOrEmpty
     }
 
+	<#
 	It "has a valid prefix" {
 		$Script:Manifest.Prefix | Should Not BeNullOrEmpty
 	}
+	#>
 
 	It "has a valid copyright" {
-		$Script:Manifest.CopyRight | Should Not BeNullOrEmpty
+		$ManifestHash.CopyRight | Should Not BeNullOrEmpty
 	}
 
 	It 'exports all public functions' {
-		$FunctionFiles = Get-ChildItem "$ModulePath\Scripts\Public" -Filter *.ps1 | Select -ExpandProperty BaseName
-		$FunctionNames = $FunctionFiles | foreach {$_ -replace '-', "-$($Script:Manifest.Prefix)"}
-		$ExFunctions = $Script:Manifest.ExportedFunctions.Values.Name
+		$ExFunctions = $ManifestHash.FunctionsToExport
+		$FunctionFiles = Get-ChildItem "$ModulePath\Functions\Public" -Filter *.ps1 | Select-Object -ExpandProperty BaseName
+		$FunctionNames = $FunctionFiles #| foreach {$_ -replace '-', "-$($Script:Manifest.Prefix)"}
+		
 		foreach ($FunctionName in $FunctionNames)
 		{
 			$ExFunctions -contains $FunctionName | Should Be $true
