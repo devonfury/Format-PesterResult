635dd9756540589110d70b14721806cc47935d0d
Marvin Carpenter <marvin.carpenter@gmail.com>
2019-07-15 16:06:06 -0500

    updatd functions

--- Functions/Private/CreateHeaderArray.ps1
+++ Functions/Private/CreateHeaderArray.ps1
@@ -1,27 +1,29 @@
-function CreateHeaderArray
-{   
-    $myArray = @()
+[CmdletBinding()]
+param
+(    
+)
 
-    if($PesterResult -is [Array])
-    {     
-        $testResults = $PesterResult[0]   
+$myArray = @()
+
+if($PesterResult -is [Array])
+{     
+    $testResults = $PesterResult[0]   
+}
+else
+{
+    $testResults = $PesterResult
+}
+
+$describe = $testResults.TestResult[0].Describe
+foreach($result in $testResults.TestResult)
+{
+    if($describe -ne $result.Describe)
+    {
+        break
     }
     else
     {
-        $testResults = $PesterResult
+        $myArray += $result
     }
-
-    $describe = $testResults.TestResult[0].Describe
-    foreach($result in $testResults.TestResult)
-    {
-        if($describe -ne $result.Describe)
-        {
-            break
-        }
-        else
-        {
-            $myArray += $result
-        }
-    }
-    write-output $myArray
 }
+write-output $myArray
